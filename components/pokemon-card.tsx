c2c4ba95eb86fb1e96383e4675730ca44c84e7f2
alaqsaka <aqsaabidin15@gmail.com>
2023-08-11 16:33:11 +0700

    pokemon card with image

--- components/pokemon-card.tsx
+++ components/pokemon-card.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Image from "next/image";
 
 interface PokemonCardProps {
   data: any;
@@ -10,15 +11,27 @@ const PokemonCard: React.FC<PokemonCardProps> = ({ data }) => {
 
   return (
     <div className="bg-emerald-100 p-2 rounded-md">
-      <p className="capitalize font-semibold mb-3">{data.name}</p>
-      {pokemonTypes.map((item: any) => (
-        <div
-          key={item.type.name}
-          className="bg-slate-50 p-2 px-4 w-fit mb-2 rounded-2xl text-xs"
-        >
-          {item.type.name}
+      <div className="flex justify-between">
+        <div>
+          <p className="capitalize font-semibold mb-3">{data.name}</p>
+          {pokemonTypes.map((item: any) => (
+            <div
+              key={item.type.name}
+              className="bg-slate-50 p-2 px-4 w-fit mb-2 rounded-2xl text-xs"
+            >
+              {item.type.name}
+            </div>
+          ))}
         </div>
-      ))}
+        <Image
+          width={80}
+          // fill
+          height={80}
+          src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${data.id}.svg`}
+          alt="Pokemon"
+          className="object-contain h-auto min-w-200 min-h-200 max-h-200"
+        />
+      </div>
     </div>
   );
 };
